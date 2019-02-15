72831066e36684c1009e1866c25490a150ce1d25
Yurii Koltso <yurkoga25@gmail.com>
2019-02-15 09:42:41 +0200

    Added require routes

--- server.js
+++ server.js
@@ -2,34 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 // Create express app
-
 const app = express();
 
-// parse req of content-type - ...urlencoded
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-// Parse json req
-
-app.use(bodyParser.json())
-
 // Configuring database
-
 const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
+// Configuring server
+const serverConfig = require('./config/server.config.js');
+
+// parse req of content-type - ...urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Parse json req
+app.use(bodyParser.json());
 
 // Connecting to db
-
 mongoose.connect(dbConfig.url, {
 	useNewUrlParser: true
 }).then(() => {
-	console.log("Successfully connected to the database");
+	console.log("Successfully connected to the dishes database");
 }).catch(err => {
 	console.log('Could not connect to the database. Exiting now...', err);
 	process.exit();
-})
+});
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -37,16 +33,17 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
 });
-// define simple route
 
-app.get('/', (req, res) => {
-	res.json({"message": "Welcome to Note app"});
-});
+// Require  routes
+require('./app/routes/home.routes.js')(app);
 
-// Require notes routes
 require('./app/routes/dish.routes.js')(app);
+
+require('./app/routes/worker.routes.js')(app);
+
+require('./app/routes/order.routes.js')(app);
 // Listen requests
 
-app.listen(3000, () => {
-	console.log("Server running on port 3000");
+app.listen(serverConfig.port, () => {
+	console.log(`Server running on port ${serverConfig.port}`);
 })
