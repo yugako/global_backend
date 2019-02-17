4e78c48a1d174467c5566f90fb3a6ab889e77e04
Yurii Koltso <yurkoga25@gmail.com>
2019-02-17 20:54:16 +0200

    Added auth routes

---
+++ app/routes/auth.js
@@ -0,0 +1,78 @@
+const mongoose = require('mongoose');
+
+const passport = require('passport');
+
+const settings = require('../../config/settings');
+
+require('../../config/passport')(passport);
+
+const express = require('express');
+
+const jwt = require('jsonwebtoken');
+
+const router = express.Router();
+
+const Worker = require("../models/worker.model.js");
+
+router.post('/register', (req, res) => {
+	if (!req.body.name || !req.body.password) {
+		res.json({
+			success: false,
+			msg: 'Please pass username and password' 
+		});
+	} else {
+		let newWorker = new Worker({
+			name: req.body.name,
+			password: req.body.password
+		});
+		// Save new worker
+		newWorker.save(err => {
+			if (err) {
+				return res.json({
+					success: false,
+					msg: 'Worker already exists'
+				});
+			}
+			res.json({
+				success: true,
+				msg: 'Successful created new user'
+			});
+		})
+	}
+});
+
+router.post('/login', (req, res) => {
+	Worker.findOne({
+		name: req.body.name
+	}, (err, worker) => {
+		if (err) {
+			throw err;
+		}
+		if (!worker) {
+			res.status(401).send({
+				success: false, 
+				msg: 'Authentication failed. User not found'
+			});
+		} else {
+			// If password mathches
+			worker.comparePassword(req.body.password, (err, isMatch) => {
+				if (isMatch && !err) {
+					// If everything ok
+					let token = jwt.sign(worker.toJSON(), settings.secret);
+					// Return info including token as Json
+					res.json({
+						success: true,
+						token: 'JWT' + token
+					});
+				} else {
+					res.status(401).send({
+						success: false,
+						msg: 'Authentication failed. Wrong password.'
+					});
+				}
+			});
+		}
+	});
+});
+
+module.exports = router;
