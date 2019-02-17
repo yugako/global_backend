90dac6faf0bb43d84c2083728dc29de5da1b3eea
Yurii Koltso <yurkoga25@gmail.com>
2019-02-17 20:53:46 +0200

    Secure order route

--- app/routes/order.routes.js
+++ app/routes/order.routes.js
@@ -1,21 +1,63 @@
 const express = require('express');
 const router = express.Router();
 
+let passport = require('passport');
+require('../../config/passport')(passport);
+
 const OrderModel = require('../models/order.model.js');
 
 const OrderController = require('../controllers/order.controller.js');
 
 const orderCtrl = new OrderController(OrderModel);
 
-router.get('/orders', orderCtrl.findAll);
-
-router.get('/orders/:itemId', orderCtrl.findOne);
+router.get('/orders', passport.authenticate('jwt', {session: false}), (req, res) => {
+	let token = getToken(req.headers);
+	if (token) {
+		orderCtrl.findAll;
+	} else {
+		return res.status(403).send({success: false, msg: 'Unauthorized'});
+	}
+});
+router.get('/orders/:itemId', passport.authenticate('jwt', {session: false}), (req, res) => {
+	let token = getToken(req.headers);
+	if (token) {
+		orderCtrl.findOne;
+	} else {
+		return res.status(403).send({success: false, msg: 'Unauthorized'});
+	}
+});
 
 router.post('/orders', orderCtrl.create);
 
-router.put('/orders/:itemId', orderCtrl.update);
+router.put('/orders/:itemId', passport.authenticate('jwt', {session: false}), (req, res) => {
+	let token = getToken(req.headers);
+	if (token) {
+		orderCtrl.update;
+	} else {
+		return res.status(403).send({success: false, msg: 'Unauthorized'});
+	}
+});
 
-router.delete('/orders/:itemId', orderCtrl.delete);
+router.delete('/orders/:itemId', passport.authenticate('jwt', {session: false}), (req, res) => {
+	let token = getToken(req.headers);
+	if (token) {
+		orderCtrl.delete;
+	} else {
+		return res.status(403).send({success: false, msg: 'Unauthorized'});
+	}
+});
 
+getToken = (headers) => {
+	if (headers && headers.authorization) {
+		let parted = headers.authorization.split(' ');
+		    if (parted.length === 2) {
+		      return parted[1];
+		    } else {
+		      return null;
+		    }
+	} else {
+		return null;
+	}
+}
 
 module.exports = router;
