e6b59661203d02f1864d86d14b075d68539e6479
Yurii Koltso <yurkoga25@gmail.com>
2019-02-17 15:54:26 +0200

    Added title, price, weight validation

--- app/routes/dish.routes.js
+++ app/routes/dish.routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const {check} = require('express-validator/check');
+
 const DishModel = require('../models/dish.model.js');
 
 const DishController = require('../controllers/dish.controller.js');
@@ -11,9 +13,17 @@ router.get('/dishes', dishCtrl.findAll);
 
 router.get('/dishes/:itemId', dishCtrl.findOne);
 
-router.post('/dishes', dishCtrl.create);
+router.post('/dishes', [
+		check('title').isLength({ min: 3 }),
+		check('price').isNumeric(),
+		check('weight').isNumeric(),
+	], dishCtrl.create);
 
-router.put('/dishes/:itemId', dishCtrl.update);
+router.put('/dishes/:itemId', [
+		check('title').isLength({ min: 3 }),
+		check('price').isNumeric(),
+		check('weight').isNumeric(),
+	], dishCtrl.update);
 
 router.delete('/dishes/:itemId', dishCtrl.delete);
 
