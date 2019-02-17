e4ca87bdf8753d03fcecd90dcd1dc0f94e5fee6c
Yurii Koltso <yurkoga25@gmail.com>
2019-02-17 20:55:06 +0200

    Added passport configuration

---
+++ config/passport.js
@@ -0,0 +1,28 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// Load up user model
+
+const WorkerModel = require('../app/models/worker.model.js');
+
+const settings = require('./settings');
+
+module.exports = function (passport) {
+	let opts = {};
+
+	opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+	opts.secretOrKey = settings.secret;
+	passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+		WorkerModel.findOne({id: jwt_payload.id}, (err, worker) => {
+			if (err) {
+				return done(err, false);
+			}
+			if (worker) {
+				done(null, worker);
+			} else {
+				done(null, false);
+			}
+		})
+	}))
+}
