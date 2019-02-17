6df6cfe9f7ef6f22af6f9bbcc7194481d5ee92d8
Yurii Koltso <yurkoga25@gmail.com>
2019-02-17 15:55:11 +0200

    Added name, password validation

--- app/routes/worker.routes.js
+++ app/routes/worker.routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const {check} = require('express-validator/check');
+
 const WorkerModel = require('../models/worker.model.js');
 
 const WorkerController = require('../controllers/worker.controller.js');
@@ -11,9 +13,17 @@ router.get('/workers', workerCtrl.findAll);
 
 router.get('/workers/:itemId', workerCtrl.findOne);
 
-router.post('/workers', workerCtrl.create);
+router.post('/workers', [
+		check('name').isLength({min: 3}),
+		check('password').isLength({min: 6})
+			.withMessage('Must be at least 6 chars long')
+			.matches(/\d/).withMessage('Must contain a number')
+	], workerCtrl.create);
 
-router.put('/workers/:itemId', workerCtrl.update);
+router.put('/workers/:itemId', [
+		check('name').isLength({min: 3}),
+		check('password').isLength({min: 6})
+	], workerCtrl.update);
 
 router.delete('/workers/:itemId', workerCtrl.delete);
 
