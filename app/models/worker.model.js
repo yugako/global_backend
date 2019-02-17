94867ea269467c73da834440a91725a4b966785f
Yurii Koltso <yurkoga25@gmail.com>
2019-02-17 20:52:07 +0200

    Added passport methods

--- app/models/worker.model.js
+++ app/models/worker.model.js
@@ -1,11 +1,49 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 
 const WorkerSchema = mongoose.Schema({
-	name: String,
-	password: String,
+	name: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
 }, {
 	timestamps: true,
 	collection : 'workers'
 });
+
+WorkerSchema.pre('save', (next) => {
+	let worker = this;
+
+	if (this.isModified('password') || this.isNew) {
+		bcrypt.genSalt(10, (err,salt) => {
+			if (err) {
+				return next(err);
+			}
+			bcrypt.hash(worker.password, salt, null, (err, hash) => {
+				if (err) {
+					return next(err);
+				}
+				worker.password = hash;
+				next();
+			})
+		})
+	} else {
+		return next();
+	}
+});
+
+WorkerSchema.methods.comparePassword = (passw, cb) => {
+	bcrypt.compare(passw, this.password, (err, isMatch) => {
+		if (err) {
+			return cb(err);
+		}
+		cb(null, isMatch);
+	})
+}
 
 module.exports = mongoose.model('Worker', WorkerSchema);
