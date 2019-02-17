605114b59e4eee0bd116e89f024f27ec6cfb85d6
Yurii Koltso <yurkoga25@gmail.com>
2019-02-17 12:53:52 +0200

    Inherit main controller

--- app/controllers/order.controller.js
+++ app/controllers/order.controller.js
@@ -1,112 +1,23 @@
-const Order = require('../models/order.model.js');
-
-class Orders {
-	create (req, res) {
-		// Validate request
-		if (!req.body) {
-			return res.status(400).send({
-				message: 'Order cannot be unnamed'
-			});
-		}
-
-		//Create a order
-		const order = new Order({
-			list: req.body.list,
-			price: req.body.price
-		});
-
-		order.save()
-			.then(data => {
-				res.send(data);
-			}).catch(err => {
-				res.status(500).send({
-					message: err.message || "Some error occured while creating new order"
-				});
-			});
+const AppController = require('../controllers/controller');
+/**
+ * The App controller class where other controller inherits or
+ * overrides pre defined and existing properties
+ */
+class OrderController extends AppController {
+	/**
+	 * @param {Model} model The default model object
+	 * for the controller. Will be required to create
+	 * an instance of the controller
+	 */
+	constructor(model) {
+		super(model);
 	}
-	findAll (req, res) {
-		Order.find()
-			.then(orders => {
-				res.send(orders);
-			}).catch(err => {
-				res.status(500).send({
-					message: err.message || "Some error occured while retrieving orders"
-				})
-			})
-	}
-	findOne (req, res)  {
-		Order.findById(req.params.orderId)
-			.then(order => {
-				if (!order) {
-					return res.status(404).send({
-						message: "Order not found id" + req.params.orderId
-					});
-				}
-
-				res.send(order);
-			}).catch(err => {
-				if(err.kind === 'ObjectId') {
-					return res.status(404).send({
-						message: 'Order not found with id ' + req.params.orderId
-					});
-				}
-
-				return res.status(500).send({
-					message: 'Error retrieving order with id ' + req.params.orderId
-				})
-			})
-	}
-	update (req, res) {
-		// Validate request
-		if (!req.body) {
-			return res.status(400).send({
-				message: 'Order content cannot be unnamed'
-			});
-		}
-		// Find order and update it with the request body
-		Order.findByIdAndUpdate(req.params.orderId, {
-			list: req.body.list,
-			price: req.body.price
-		}, {new: true})
-		.then(order => {
-			if (!order) {
-				return res.status(404).send({
-					message: "Order not found with id " + req.params.orderId
-				});
-			}
-			res.send(order);
-		}).catch(err => {
-			if (err.kind === 'ObjectId') {
-				return res.status(404).send({
-					message: "Order not found with id " + req.params.orderId
-				})
-			}
-
-			return res.status(500).send({
-				message: 'Error updating order with id ' + req.params.orderId
-			})
-		})
-	}
-	delete (req, res) {
-		Order.findByIdAndRemove(req.params.orderId)
-			.then(order => {
-				if (!order) {
-					return res.status(404).send({
-						message: "Order not found with id " + req.params.orderId
-					})
-				}
-				res.send({message: 'Order deleted successfully'});
-			}).catch(err => {
-				if(err.kind === 'ObjectId' || err.name === 'NotFound') {
-					return res.status(404).send({
-						message: "Order not found with id " + req.params.orderId
-					})
-				}
-				return res.status(500).send({
-		            message: "Could not delete order with id " + req.params.orderId
-		        });
-			})
-	}
+	/**
+	 * @param {Object} req The request object
+	 * @param {Object} res The response object
+	 * @param {function} next The callback to the next program handler
+	 * @return {Object} res The response object
+	 */
 }
 
-module.exports = Orders;
+module.exports =  OrderController;
--- app/controllers/worker.controller.js
+++ app/controllers/worker.controller.js
@@ -1,116 +1,23 @@
-const Worker = require('../models/worker.model.js');
-
-class Workers {
-	// Create and save new worker
-	create (req, res) {
-		// Validate request
-		if (!req.body.name) {
-			return res.status(400).send({
-				message: 'Worker cannot be unnamed'
-			});
-		}
-
-		//Create a worker
-		const worker = new Worker({
-			name: req.body.name,
-			password: req.body.password,
-		});
-
-		worker.save()
-			.then(data => {
-				res.send(data);
-			}).catch(err => {
-				res.status(500).send({
-					message: err.message || "Some error occured while creating new worker"
-				});
-			});
+const AppController = require('../controllers/controller');
+/**
+ * The App controller class where other controller inherits or
+ * overrides pre defined and existing properties
+ */
+class WorkerController extends AppController {
+	/**
+	 * @param {Model} model The default model object
+	 * for the controller. Will be required to create
+	 * an instance of the controller
+	 */
+	constructor(model) {
+		super(model);
 	}
-	// Retrieve and return all workeres from db
-	findAll (req, res) {
-		Worker.find()
-			.then(workers => {
-				res.send(workers);
-			}).catch(err => {
-				res.status(500).send({
-					message: err.message || "Some error occured while retrieving workers"
-				})
-			})
-	}
-	// Find a single worker with workerId
-	findOne (req, res) {
-		Worker.findById(req.params.workerId)
-			.then(worker => {
-				if (!worker) {
-					return res.status(404).send({
-						message: "Worker not found id" + req.params.workerId
-					});
-				}
-
-				res.send(worker);
-			}).catch(err => {
-				if(err.kind === 'ObjectId') {
-					return res.status(404).send({
-						message: 'Worker not found with id ' + req.params.workerId
-					});
-				}
-
-				return res.status(500).send({
-					message: 'Error retrieving worker with id ' + req.params.workerId
-				})
-			})
-	}
-	// Update selected worker
-	update (req, res) {
-		// Validate request
-		if (!req.body.name) {
-			return res.status(400).send({
-				message: 'Worker content cannot be unnamed'
-			});
-		}
-		// Find worker and update it with the request body
-		Worker.findByIdAndUpdate(req.params.workerId, {
-			name: req.body.name,
-			password: req.body.password,
-		}, {new: true})
-		.then(worker => {
-			if (!worker) {
-				return res.status(404).send({
-					message: "Worker not found with id " + req.params.workerId
-				});
-			}
-			res.send(worker);
-		}).catch(err => {
-			if (err.kind === 'ObjectId') {
-				return res.status(404).send({
-					message: "Worker not found with id " + req.params.workerId
-				})
-			}
-
-			return res.status(500).send({
-				message: 'Error updating worker with id ' + req.params.workerId
-			})
-		})
-	}
-	// Delete worker
-	delete (req, res) {
-		Worker.findByIdAndRemove(req.params.workerId)
-			.then(worker => {
-				if (!worker) {
-					return res.status(404).send({
-						message: "Worker not found with id " + req.params.workerId
-					})
-				}
-				res.send({message: 'Worker deleted successfully'});
-			}).catch(err => {
-				if(err.kind === 'ObjectId' || err.name === 'NotFound') {
-					return res.status(404).send({
-						message: "Worker not found with id " + req.params.workerId
-					})
-				}
-				return res.status(500).send({
-		            message: "Could not delete worker with id " + req.params.workerId
-		        });
-			})
-	}
+	/**
+	 * @param {Object} req The request object
+	 * @param {Object} res The response object
+	 * @param {function} next The callback to the next program handler
+	 * @return {Object} res The response object
+	 */
 }
-module.exports = Workers;
+
+module.exports =  WorkerController;
