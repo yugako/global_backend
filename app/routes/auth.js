092ccf74bd8497dac004ed1b8ab43c35cf7b7125
Yurii Koltso <yurkoga25@gmail.com>
2019-02-20 15:34:58 +0200

    Added admin login route

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -14,6 +14,9 @@ const router = express.Router();
 
 const Worker = require("../models/worker.model.js");
 
+const Admin = require("../models/admin.model.js");
+
+
 router.post('/register', function (req, res) {
 	if (!req.body.name || !req.body.password) {
 		res.json({
@@ -60,6 +63,42 @@ router.post('/login', function (req, res) {
 				if (isMatch && !err) {
 					// If everything ok
 					let token = jwt.sign(worker.toJSON(), settings.secret);
+					// Return info including token as Json
+					res.json({
+						success: true,
+						token: 'JWT ' + token,
+						name: req.body.name,
+						role: req.body.role
+					});
+				} else {
+					res.status(401).send({
+						success: false,
+						msg: 'Authentication failed. Wrong password.'
+					});
+				}
+			});
+		}
+	});
+});
+
+router.post('/login-admin', function (req, res) {
+	Admin.findOne({
+		name: req.body.name
+	}, function (err, admin) {
+		if (err) {
+			throw err;
+		}
+		if (!admin) {
+			res.status(401).send({
+				success: false, 
+				msg: 'Authentication failed. User not found'
+			});
+		} else {
+			// If password mathches
+			admin.comparePassword(req.body.password, function (err, isMatch) {
+				if (isMatch && !err) {
+					// If everything ok
+					let token = jwt.sign(admin.toJSON(), settings.secret);
 					// Return info including token as Json
 					res.json({
 						success: true,
