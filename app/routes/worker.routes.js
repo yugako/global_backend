5ae78617612922510b6f487d7372f026b521b025
Yurii Koltso <yurkoga25@gmail.com>
2019-02-20 18:06:33 +0200

    Protect worker routes

--- app/routes/worker.routes.js
+++ app/routes/worker.routes.js
@@ -3,29 +3,32 @@ const router = express.Router();
 
 const {check} = require('express-validator/check');
 
+let passport = require('passport');
+require('../../config/passport')(passport);
+
 const WorkerModel = require('../models/worker.model.js');
 
 const WorkerController = require('../controllers/worker.controller.js');
 
 const workerCtrl = new WorkerController(WorkerModel);
 
-router.get('/workers', workerCtrl.findAll);
+router.get('/workers', passport.authenticate('jwt', {session: false}), workerCtrl.findAll);
 
-router.get('/workers/:itemId', workerCtrl.findOne);
+router.get('/workers/:itemId', passport.authenticate('jwt', {session: false}), workerCtrl.findOne);
 
-router.post('/workers', [
+router.post('/workers', passport.authenticate('jwt', {session: false}), [
 		check('name').isLength({min: 3}),
 		check('password').isLength({min: 6})
 			.withMessage('Must be at least 6 chars long')
 			.matches(/\d/).withMessage('Must contain a number')
 	], workerCtrl.create);
 
-router.put('/workers/:itemId', [
+router.put('/workers/:itemId', passport.authenticate('jwt', {session: false}), [
 		check('name').isLength({min: 3}),
 		check('password').isLength({min: 6})
 	], workerCtrl.update);
 
-router.delete('/workers/:itemId', workerCtrl.delete);
+router.delete('/workers/:itemId', passport.authenticate('jwt', {session: false}), workerCtrl.delete);
 
 
 module.exports = router;
