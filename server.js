fc835d1c9d7303c1a88d4e695e07180b2fd7cba1
Yurii Koltso <yurkoga25@gmail.com>
2019-02-21 22:28:55 +0200

    Use passport-local auth

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const passport = require('passport');
+
+const LocalStrategy = require('passport-local').Strategy;
+const cookieParser = require('cookie-parser');
 
 // CORS
 const cors = require('cors');
-
-// Configuring database
-const mongooseConnection = require('./config/mongoose-connect.js');
 
 // Dishes routes
 const dishes = require('./app/routes/dish.routes.js');
@@ -13,43 +16,61 @@ const dishes = require('./app/routes/dish.routes.js');
 // Orders routes
 const orders = require('./app/routes/order.routes.js');
 
-// Workers routes
-const workers = require('./app/routes/worker.routes.js');
-
-// Admins routes
-const admins = require('./app/routes/admin.routes.js');
-
 // Auth routes
 const auth = require('./app/routes/auth');
+
+mongoose.connect('mongodb://127.0.0.1:27017/service')
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
 
 // Create express app
 const app = express();
 
 // Configuring server
-
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config/config')[env];
 
-// parse req of content-type - ...urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
-
 // Parse json req
 app.use(bodyParser.json());
-
-// Connect to db
-app.use(function (req, res, next) {
-  mongooseConnection();
-  next();
-});
-
+// parse req of content-type - ...urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
+
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Require  routes
 app.use('/', dishes);
 app.use('/', orders);
-app.use('/', workers);
-app.use('/', admins);
 app.use('/', auth);
+
+
+// passport configuration
+const User = require('./app/models/worker.model.js');
+
+passport.use(new LocalStrategy({
+	passReqToCallback: true
+},
+  function(req,username, password, done) {
+    User.findOne({ username: username, role: req.body.role}, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (user.password !== password || user.role !== req.body.role) { 
+      	return done(null, false); 
+      }
+      return done(null, user);
+    });
+  }
+));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Listen requests
 
