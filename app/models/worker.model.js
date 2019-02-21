91fdbf9331810d4618fc48c956b074403543f448
Yurii Koltso <yurkoga25@gmail.com>
2019-02-21 22:26:37 +0200

    Added username field to schema

--- app/models/worker.model.js
+++ app/models/worker.model.js
@@ -1,53 +1,16 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt-nodejs');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
 
-const WorkerSchema = mongoose.Schema({
-	name: {
-		type: String,
-		unique: true,
-		required: true
-	},
-	password: {
-		type: String,
-		required: true
-	},
-	role: {
-		type: String,
-		required: true
-	}
+var UserSchema = new Schema({
+	username: String,
+    name: String,
+    password: String,
+    role: String
 }, {
-	timestamps: true,
 	collection : 'workers'
 });
 
-WorkerSchema.pre('save', function (next) {
-	let worker = this;
+UserSchema.plugin(passportLocalMongoose);
 
-	if (this.isModified('password') || this.isNew) {
-		bcrypt.genSalt(10, (err,salt) => {
-			if (err) {
-				return next(err);
-			}
-			bcrypt.hash(worker.password, salt, null, function (err, hash) {
-				if (err) {
-					return next(err);
-				}
-				worker.password = hash;
-				next();
-			})
-		})
-	} else {
-		return next();
-	}
-});
-
-WorkerSchema.methods.comparePassword = function (passw, cb) {
-	bcrypt.compare(passw, this.password, function (err, isMatch) {
-		if (err) {
-			return cb(err);
-		}
-		cb(null, isMatch);
-	})
-}
-
-module.exports = mongoose.model('Worker', WorkerSchema);
+module.exports = mongoose.model('User', UserSchema);
