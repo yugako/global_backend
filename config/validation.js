c508acfb13a2dcdf8832219f1411c2733f624cfb
Yurii Koltso <yurkoga25@gmail.com>
2019-02-23 12:51:41 +0200

    Added input validations

--- config/validation.js
+++ config/validation.js
@@ -2,15 +2,27 @@ const {check} = require('express-validator/check');
 
 const Validation = {
 	dishes: [
-		check('title').isLength({ min: 3 }),
-		check('price').isNumeric(),
-		check('weight').isNumeric(),
+		check('title').isLength({ min: 3 })
+			.withMessage('must be at least 3 chars long'),
+		check('excerpt').isLength({ min: 5 })
+			.withMessage('must be at least 5 chars long'),
+		check('description').isLength({ min: 10 })
+			.withMessage('must be at least 10 chars long'),
+		check('ingradients').isLength({ min: 5 })
+			.withMessage('must be at least 5 chars long'),
+		check('price').isNumeric()
+			.withMessage('must be a number value'),
+		check('weight').isNumeric()
+			.withMessage('must be a number value'),
 	],
 	auth: [
-		check('name').isLength({min: 3}),
+		check('name').isLength({min: 3})
+			.withMessage('must be at least 3 chars long'),
+		check('username').isLength({min: 3})
+			.withMessage('must be at least 3 chars long'),
 		check('password').isLength({min: 6})
-			.withMessage('Must be at least 6 chars long')
-			.matches(/\d/).withMessage('Must contain a number')
+			.withMessage('must be at least 6 chars long')
+			.matches(/\d/).withMessage('must contain a number')
 	],
 }
 
