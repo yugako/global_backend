ead190fca97abf36ef98afad8777f1dca6500c58
Yurii Koltso <yurkoga25@gmail.com>
2019-02-25 19:59:32 +0200

    Refactored code

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
 const winston = require('./config/winston');
 const config = require('config');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-
 const app = express();
 
 const mongoConnect = require('./config/mongoose-connect.js');
@@ -15,29 +9,14 @@ const passportConfig = require('./config/passport.js');
 
 const routesInit = require('./app/routes/index');
 
+const appMiddleware = require('./app/appMiddleware');
+
 const serverConfig = config.get('Development.serverConfig');
-
+appMiddleware(app);
 mongoConnect();
-
-app.use(morgan('combined', { stream: winston.stream }));
-app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(cors());
-
-app.use(cookieParser());
-
-app.use(require('express-session')({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
-}));
-
 
 passportConfig(app);
 routesInit(app);
-
 
 app.listen(serverConfig.port, () => {
 	winston.log("info", `App listening at ${serverConfig.port}`);
