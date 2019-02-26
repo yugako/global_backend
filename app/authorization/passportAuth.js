e89f26ba03990426571e38c77d99383571b614fb
Yurii Koltso <yurkoga25@gmail.com>
2019-02-26 16:31:42 +0200

    Added auth files

--- app/authorization/passportAuth.js
+++ app/authorization/passportAuth.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 
 const LocalStrategy = require('passport-local').Strategy;
 
-const User = require('../app/models/worker.model.js');
+const User = require('../models/worker.model.js');
 
 
 module.exports = function (app) {
